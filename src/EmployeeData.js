9f4a9950596d117674560e211b8146e570511a28
Saloni Tanna <salonitanna6@gmail.com>
2024-04-12 21:14:15 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,21 @@
+export const EmployeeData = [
+    {
+        id:1,
+        firstName:'Saloni',
+        lastName:'Tanna',
+        age:22
+    },
+    {
+        id:2,
+        firstName:'Vinisha',
+        lastName:'Shah',
+        age:20
+    },
+    {
+        id:3,
+        firstName:'Meet',
+        lastName:'Sharma',
+        age:22
+    },
+
+]
